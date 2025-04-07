efd00f7fd4f361643e295782eb5dfd1ca5fa0480
Felipe Mayer <65250731+felipefoc@users.noreply.github.com>
2025-04-07 14:38:53 -0300

    porta braguilha 2

--- server.py
+++ server.py
@@ -83,4 +83,4 @@ def check_input(request: CheckInputRequest, db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8100)
